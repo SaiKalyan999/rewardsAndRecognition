224ebaeab98aab24c35469f47ff572561dd877b9
Naman Arora <naman.arora@nineleaps.com>
2021-02-01 20:23:54 +0530

    i

--- src/main/java/com/nineleaps/rewardandrecognition/RewardsAndRecognition.java
+++ src/main/java/com/nineleaps/rewardandrecognition/RewardsAndRecognition.java
@@ -1,12 +1,23 @@
 package com.nineleaps.rewardandrecognition;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+@ComponentScan(basePackages={"com.nineleaps.rewardandrecognition.controller"})
+
 public class RewardsAndRecognition {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RewardsAndRecognition.class, args);
+       // try {} catch (Exception e) {
+         //   e.printStackTrace();
+        //}
     }
 }
